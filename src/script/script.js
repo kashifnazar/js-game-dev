292a21d6a71cb90b55690fb7762deef4e74995ff
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-08-01 17:38:38 +0100

    Handling of different modes

--- src/script/script.js
+++ src/script/script.js
@@ -5,6 +5,13 @@ const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 600
 const PLAYER_IMAGE = './static/images/shadow_dog.png'
 
+let mode = 'idle'
+
+const modeSelect = document.getElementById('mode-select')
+
+modeSelect.addEventListener('change', (e) => mode = e.target.value)
+
+
 const playerImage = new Image()
 playerImage.src = PLAYER_IMAGE
 
@@ -22,20 +29,71 @@ const PLAYER_STATES = [
     {   
         name: 'idle',
         frames: 7
-    }, {
+    }, 
+    {
         name: 'jump',
         frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'gethit',
+        frames: 4
     }
 ]
+
+let playerPositions = {}
+
+PLAYER_STATES.forEach((state, index) => {
+    
+    const loc = []
+
+    for(let j = 0; j < state.frames; j++) {
+        loc.push({
+            x: j * SPRITE_WIDTH,
+            y: index * SPRITE_HEIGHT,
+        })
+    }
+
+    playerPositions[state.name] = loc
+})
+
+console.log(playerPositions);
+
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-    let position = Math.floor(gameFrame/STAGGER_FRAMES) % 10
-    let frameX = position * SPRITE_WIDTH
+    let position = Math.floor(gameFrame/STAGGER_FRAMES) % playerPositions[mode].length
     
-    
-    ctx.drawImage(playerImage, frameX, playerY * SPRITE_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT)
+    ctx.drawImage(playerImage, playerPositions[mode][position].x, playerPositions[mode][position].y, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT)
+
 
     gameFrame++
 
