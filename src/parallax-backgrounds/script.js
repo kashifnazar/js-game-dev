276a439bf458fc9f23b70f80f78462c95ce6aedb
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-08-05 00:46:21 +0100

    Acheived layering effect

--- src/parallax-backgrounds/script.js
+++ src/parallax-backgrounds/script.js
@@ -2,40 +2,47 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 
 const CANVAS_WIDTH = canvas.width = 800
-const CANVAS_HEIGHT = canvas.height = 600
+const CANVAS_HEIGHT = canvas.height = 700
 
 const BACKGROUND_WIDTH = 2400
 
-const GAME_SPEED = 10
+const GAME_SPEED = 5
 
 class Layer {
 
-    constructor(image, speed) {
+    constructor(image, speed = 0.2) {
         this.x = 0
+        this.width = 2400
+        this.height = 700
+        
+        this.speed = speed
 
         this.image = new Image()
         this.image.src = image
     }
 
     draw() {
-        ctx.drawImage(this.image, this.x, 0)
-        ctx.drawImage(this.image, this.x + BACKGROUND_WIDTH, 0)
+        ctx.drawImage(this.image, this.x, 0, this.width, this.height)
+        ctx.drawImage(this.image, this.x + BACKGROUND_WIDTH, 0, this.width, this.height)
     }
 
     update() {
-        this.x = this.x % BACKGROUND_WIDTH - GAME_SPEED
-        console.log(this.x);
+        const layerSpeed = GAME_SPEED * this.speed
+        this.x = this.x % BACKGROUND_WIDTH - layerSpeed
     }
 }
 
-const layer = new Layer(`../../static/images/layer-4.png`)
+const layers = [1,2,3,4,5].map(n => new Layer(`../../static/images/layer-${n}.png`, n * .2))
 
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-    layer.draw()
-    layer.update()
+    layers.forEach(layer => {
+        layer.draw()
+        layer.update()
+    })
+    
 
     requestAnimationFrame(animate)
 }
