5e7f63a37e79d12772b3f3bd49b8131cfef60130
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-08-08 15:38:33 +0100

    Circle around mouse

---
+++ src/circles-collision/script.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById('canvas1')
+
+/** @type {CanvasRenderingContext2D} */
+const ctx = canvas.getContext('2d')
+
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 800
+
+let mouseX, mouseY = 0
+
+canvas.addEventListener('mousemove', e => {
+    mouseX = e.offsetX
+    mouseY = e.offsetY
+})
+
+
+class Circle {
+    
+    constructor(x, y, radius, fixed = true) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.fixed = fixed
+    }
+
+    udpate() {
+        if(!this.fixed) {
+            this.x = mouseX
+            this.y = mouseY
+        }
+        return this
+    }
+    
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        ctx.stroke()
+    }
+}
+
+const circles = [new Circle(100, 100, 50), new Circle(500, 500, 50, false)]
+
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+    circles.forEach(c => {
+        c.udpate().draw()
+    })
+
+    requestAnimationFrame(animate)
+}
+
+animate()
