b2ecfb1c10ef62e8aea180f53136aebc671a5cf9
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-08-08 14:39:24 +0100

    Rotatory movements

--- src/enemy-movements/script.js
+++ src/enemy-movements/script.js
@@ -7,55 +7,54 @@ const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 800
 
 
-const SPRITE_WIDTH = 266
+const SPRITE_WIDTH = 218
 const SPRITE_HEIGHT = 188
 
 let frame = 0
 
 class Enemy {
     constructor() {
-        this.x = (Math.random() * CANVAS_WIDTH)
-        this.y = Math.random() * CANVAS_HEIGHT
 
-        this.xSpeed = -Math.random() * 10
-        // this.ySpeed = Math.random() * 10 - 8
+        this.width = SPRITE_WIDTH/2
+        this.height = SPRITE_HEIGHT/2
+
+        this.x = Math.random() * (canvas.width - this.width)
+        this.y = Math.random() * (canvas.height - this.height)
 
         this.image = new Image()
-        this.image.src = '../../static/images/enemy2.png'
+        this.image.src = '../../static/images/enemy3.png'
 
         this.flap = 0
         this.flapFrame = 0
 
         this.angle = 0
-        this.angleSpeed = Math.random() * .2
-
-        this.curve = 5
+        this.angleSpeed = Math.random() * 1.5 + 0.5
+        this.curve = Math.random() * 200
 
     }
 
     draw() {
-        ctx.drawImage(this.image, this.flap * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT, this.x, this.y, SPRITE_WIDTH, SPRITE_HEIGHT )
+        ctx.drawImage(this.image, this.flap * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT, this.x, this.y, 100, 100 )
     }
 
     update() {
-        this.x += this.xSpeed
-        this.y += this.curve * Math.sin(this.angle)
 
+        this.x = this.curve * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 + this.width/2)
+        this.y = this.curve * Math.cos(this.angle * Math.PI/180) + (canvas.height/2 + this.height/2)
         
+
+        this.angle += this.angleSpeed
+
         if(++this.flapFrame % 6 === 0) this.flap++
 
         this.flap %= 6
 
-        this.angle += this.angleSpeed
         
     }
 }
 
 
 const enemies = [...Array(100)].map(e => new Enemy())
-
-console.log(enemies);
-
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
